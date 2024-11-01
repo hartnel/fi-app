24b1610119e66c351c11b22234632a7b006f361e
hartnel <kritikostamghuo@gmail.com>
2024-11-01 21:57:28 +0100

    ok

--- sectors/admin.py
+++ sectors/admin.py
@@ -3,7 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Sector, SectorType
 from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
 
-admin.site.register(Sector , TreeAdmin)
+class CustomTreeAdmin(TreeAdmin):
+    form = movenodeform_factory(Sector)
+    list_display = ('label', 'type', 'path', 'depth', 'numchild')
+    list_display_links = ('label',)
+    
+    #noo need to provide (path, depth, numchild) in readonly_fields
+    readonly_fields = ('path', 'depth', 'numchild')
+
+admin.site.register(Sector , CustomTreeAdmin)
 admin.site.register(SectorType)
