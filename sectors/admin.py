6a2eece19fbc3b9d9ba350d345376d514e91854b
hartnel <kritikostamghuo@gmail.com>
2024-11-01 21:53:44 +0100

    add tree admin

--- sectors/admin.py
+++ sectors/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Sector, SectorType
+from treebeard.admin import TreeAdmin
 
 
-admin.site.register(Sector)
+admin.site.register(Sector , TreeAdmin)
 admin.site.register(SectorType)
