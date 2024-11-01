6e2637429eb2b6650af35c2bacf0930408e4aa00
hartnel <kritikostamghuo@gmail.com>
2024-11-01 22:00:47 +0100

    some changes

--- churchs/admin.py
+++ churchs/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Church
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-admin.site.register(Church)
+
+class CustomTreeAdmin(TreeAdmin):
+    form = movenodeform_factory(Church)
+    list_display = ('name', 'path', 'depth', 'numchild')
+    list_display_links = ('name',)
+    
+    #noo need to provide (path, depth, numchild) in readonly_fields
+    readonly_fields = ('path', 'depth', 'numchild')
+
+
+admin.site.register(Church , CustomTreeAdmin)
