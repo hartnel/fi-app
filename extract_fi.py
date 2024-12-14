738018540875817583cc7555ecd4bf251224437f
hartnel <kritikostamghuo@gmail.com>
2024-12-14 15:43:22 +0100

    changes

--- extract_fi.py
+++ extract_fi.py
@@ -9,18 +9,28 @@ for i, fi in enumerate(all_fis):
     location = fi.location
     lat=""
     lon=""
+    location_label = ""
+    quater_label = ""
+    sector_label = ""
     if location:
         coords = location.location.coords[::-1]
         lat = coords[0]
         lon = coords[1]
+        location_label = fi.sector.label
+
+    if fi.quater:
+        quater_label = fi.quater.label
+        
+    if fi.sector:
+        sector_label = fi.sector.label
 
     item = {
         "number": i+1,
         "name" : fi.name,
-        "sector" : fi.sector.label,
-        "location_description" :fi.location.label,
+        "sector" : sector_label,
+        "location_description" :location_label,
         "location_gps" : f"{lat},{lon}",
-        "quater" : fi.quater.label if fi.quater else "",
+        "quater" : quater_label,
     }
     items.append(item)
 
